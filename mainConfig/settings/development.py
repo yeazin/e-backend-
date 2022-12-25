12592d80812a0e2f229cf3a671bee215d995ab74
yeazin <naz.yeasin@gmail.com>
2022-12-26 00:27:36 +0600

    Docker & postgres config

--- mainConfig/settings/development.py
+++ mainConfig/settings/development.py
@@ -12,6 +12,20 @@ DATABASES = {
     }
 }
 
+"""
+Postgres setup 
+"""
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 INSTALLED_APPS += [
     ## third party app
