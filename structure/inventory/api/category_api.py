b61b86a97b9d973d6dffd38469497e3e520b4d89
yeazin <naz.yeasin@gmail.com>
2022-12-26 19:31:00 +0600

    Category views added

--- structure/inventory/api/category_api.py
+++ structure/inventory/api/category_api.py
@@ -1,13 +1,16 @@
 """
 Category serializers
 """
-
+from rest_framework import serializers
 from structure.inventory.models import Category
 from structure.core.base_serializer import BaseSerializer
 
 
 class CategorySerializer(BaseSerializer):
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.filter(is_active=True)
+    )
+
     class Meta:
         model = Category
         fields = BaseSerializer.Meta.fields + ("name", "slug", "parent")
-        depth = 1
